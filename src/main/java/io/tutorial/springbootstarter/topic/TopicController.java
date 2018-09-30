95074144d03662d6be7aa546d6e66861b8612946
satabdi.chakraborty <satabdi.d@gmail.com>
2018-09-30 15:48:08 -0400

    Added Update
    Added Update

--- src/main/java/io/tutorial/springbootstarter/topic/TopicController.java
+++ src/main/java/io/tutorial/springbootstarter/topic/TopicController.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
-	
+
 	@RequestMapping("/topic")
 	public String getTopics() {
 		return "topic";
@@ -23,14 +25,32 @@ public class TopicController {
 	@GetMapping("/topics")
 	@ResponseBody
 	public List<Topic> getAllTopics() {
-	
+
 		return topicService.getAllTopics();
 	}
-	
+
 	@GetMapping("/topics/{topicId}")
 	public Topic getTopicByID(@PathVariable String topicId) {
-	    System.out.println("Entering getTopicByID with "+topicId);
+		System.out.println("Entering getTopicByID with " + topicId);
 		return topicService.getTopicByID(topicId);
 	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic t) {
+		System.out.println("Entering addTopic with " + t);
+		topicService.addTopic(t);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}")
+	public void removeTopic(@PathVariable String topicId) {
+		System.out.println("Entering removeTopic with topicId = " + topicId);
+		topicService.removeTopic(topicId);
+	}
 	
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}")
+	public void updateTopic(@RequestBody Topic topic) {
+		System.out.println("Entering updateTopic with = " + topic);
+		topicService.updateTopic(topic);
+	}
+
 }
--- src/main/java/io/tutorial/springbootstarter/topic/TopicService.java
+++ src/main/java/io/tutorial/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.tutorial.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,31 +9,59 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+	   new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+	   new Topic("Java", "Java", "Java Description"),
+	   new Topic("JavaScript", "Java Script", "Description"),
+	   new Topic("TypeScript", "Type Script", "Description")
+	   ));
 
-			new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("Java", "Java", "Java Description"), 
-			new Topic("JavaScript", "Java Script", "Description"),
-			new Topic("TypeScript", "Type Script", "Description"));
-	
-	
 	public List<Topic> getAllTopics() {
-		System.out.println("Entering getAllTopics "+topics);
+		System.out.println("Entering getAllTopics " + topics);
 		return topics;
 	}
-	
+
 	public Topic getTopicByID(String id) {
-		System.out.println("Entering getTopicByID with "+id);
-		
-		Topic topicByID=null;
-		
+		System.out.println("Entering TopicService:getTopicByID with " + id);
+
+		Topic topicByID = null;
+
 		for (Topic topic : topics) {
-			if(topic.getId().equals(id)) {
+			if (topic.getId().equals(id)) {
 				topicByID = topic;
 			}
 		}
-		System.out.println("Returning topic "+topicByID);
+		System.out.println("Returning from TopicService topic " + topicByID);
 		return topicByID;
+	}
+
+	public void addTopic(Topic topic) {
+		System.out.println("Entering TopicService:addTopic with " + topic);
+		topics.add(topic);
+	}
+
+	public void removeTopic(String id) {
+		System.out.println("Entering TopicService:removeTopic with " + id);
+
+		Topic topicByID = null;
+
+		for (Topic topic : topics) {
+			if (topic.getId().equals(id)) {
+				topicByID = topic;
+			}
+		}
+
+		topics.remove(topicByID);
+		System.out.println("Final Topic List from TopicService After Removal " + topics);
+	}
+
+	public void updateTopic(Topic topicToUpdate) {
+		System.out.println("Entering TopicService:updateTopic with " + topicToUpdate);
+
+		removeTopic(topicToUpdate.getId());
+		addTopic(topicToUpdate);
+		
+		System.out.println("Final Topic List After Removal " + topics);
 	}
 
 }
